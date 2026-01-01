ce28dcd649ff092ef853c5f4b93ecf562514a4f9
Ivo Bellin Salarin <ivo@nilleb.com>
2026-01-01 09:13:21 +0100

    fix: build the bundle

--- frontend/reactapp/vite.config.js
+++ frontend/reactapp/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       scope: '/ide/',
       injectManifest: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,tar}'],
+        maximumFileSizeToCacheInBytes: 3700000
       },
     }),
   ],
